8daf7b7ec92bab8d75ecebee7ee2e5937792e88a
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-06-08 12:26:21 +0200

    Add project files.

---
+++ Chapter 9 - Reading Swindler's plan/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_9___Reading_Swindler_s_plan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
